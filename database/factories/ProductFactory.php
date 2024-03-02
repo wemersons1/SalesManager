e19a292501ba44bcde04b83d96b0fb3e8005c031
wemersons1 <wemersons@live.com>
2024-03-02 17:47:34 -0300

    feat: criação de seed para popular banco de dados

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as Faker;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'price' => fake()->randomFloat(2, 0, 1000),
+            'description' => fake()->paragraph(),  
+        ];
+    }
+}
